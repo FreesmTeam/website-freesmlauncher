bd42e408290f7d00deb2252eb83db33f80667ea5
windstone-aristotle-yellow <notwindstone@gmail.com>
2024-12-30 19:43:32 +0300

    feat: add links

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -2,27 +2,30 @@ import Image from "next/image";
 import freesmLogo from '../../../public/freesm-launcher-logo.webp';
 import Link from "next/link";
 import {Icon} from "@iconify/react";
+import {GITHUB_LINK} from "@/configs/constants";
 
 export default function Header() {
     return (
         <header className="p-4 mx-auto max-w-[1280px] w-full">
             <div className="flex justify-between items-center h-12 w-full">
-                <Image height={48} src={freesmLogo} alt="FreesmLauncher logo" />
+                <Link href="/">
+                    <Image height={48} src={freesmLogo} alt="FreesmLauncher logo" />
+                </Link>
                 <div className="flex gap-4">
                     <Link
                         className="font-semibold text-white py-1 transition border-b-2 border-transparent hover:border-[#cba6f7]"
-                        href="/"
+                        href="/download"
                     >
                         Download
                     </Link>
                     <Link
                         className="font-semibold text-white py-1 transition border-b-2 border-transparent hover:border-[#cba6f7]"
-                        href="/"
+                        href="/about"
                     >
                         About
                     </Link>
                     <div className="w-[2px] bg-[#cba6f7]" />
-                    <Link href='/'>
+                    <Link target="_blank" href={GITHUB_LINK}>
                         <Icon
                             className="transition text-white hover:text-[#cba6f7]"
                             height={32}
