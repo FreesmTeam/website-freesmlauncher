42d61aa5e8eb42b3efc7e99ba871a94a677a479f
windstone-aristotle-yellow <notwindstone@gmail.com>
2024-12-30 21:50:41 +0300

    feat: implement windows header for launcher simulation

--- src/components/Launcher/Launcher.tsx
+++ src/components/Launcher/Launcher.tsx
@@ -17,9 +17,25 @@ export default function Launcher() {
                 </div>
                 <div className="flex h-full gap-0 items-stretch">
                     <div className="group flex justify-center items-center transition w-12 hover:bg-[#e5e5e5]">
-                        <div className="transition w-[10px] h-[1px] bg-[#999] group-hover:bg-[#000]" />
+                        <div
+                            className="transition w-[10px] h-[1px] bg-[#999] group-hover:bg-[#000]"
+                        />
                     </div>
-                    <div className="transition w-12 hover:bg-[#e5e5e5]" />
+                    <div className="group flex justify-center items-center transition w-12 hover:bg-[#e5e5e5]">
+                        <div
+                            className="transition w-[10px] h-[10px] border-[1px] border-[#999] group-hover:border-[#000]"
+                        />
+                    </div>
+                    <div className="group flex justify-center items-center transition w-12 hover:bg-[#e81123]">
+                        <div className="relative right-[6px]">
+                            <div
+                                className="absolute top-[50%] left-[50%] rotate-45 transition w-[13px] h-[1px] bg-[#999] group-hover:bg-[#fff]"
+                            />
+                            <div
+                                className="absolute top-[50%] left-[50%] rotate-[135deg] transition w-[13px] h-[1px] bg-[#999] group-hover:bg-[#fff]"
+                            />
+                        </div>
+                    </div>
                 </div>
             </div>
             <div></div>
