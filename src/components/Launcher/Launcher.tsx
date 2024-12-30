a6612e06cff7943446c2def111b4ecd1bbd56a02
windstone-aristotle-yellow <notwindstone@gmail.com>
2024-12-30 22:10:59 +0300

    feat: implement some launcher ui

--- src/components/Launcher/Launcher.tsx
+++ src/components/Launcher/Launcher.tsx
@@ -1,7 +1,11 @@
 import Image from "next/image";
 import freesmLogo from '../../../public/favicon.webp';
+import {LAUNCHER_TABS} from "@/configs/launcher";
+import {useTranslations} from "next-intl";
 
 export default function Launcher() {
+    const translate = useTranslations('Translations');
+
     return (
         <div className="w-full flex flex-col gap-0">
             <div className="flex justify-between items-center w-full h-8 bg-white">
@@ -38,7 +42,33 @@ export default function Launcher() {
                     </div>
                 </div>
             </div>
-            <div></div>
+            <div>
+                <div className="w-full flex flex-col gap-0">
+                    <div className="flex justify-between p-2.5 h-14 w-full bg-[#11111b]">
+                        <div className="flex items-stretch gap-2">
+                            <div className="cursor-move w-[5px] rounded-full bg-[#dbcafe]" />
+                            {
+                                LAUNCHER_TABS.map((tab) => {
+                                   return (
+                                       <button
+                                           key={tab.name}
+                                           className="cursor-default px-2 rounded-md flex items-center gap-1 hover:bg-[#211e2f]"
+                                       >
+                                           {tab.icon}
+                                           <p className="text-[13px] text-[#cdd6f4]">
+                                               {translate(tab.name)}
+                                           </p>
+                                       </button>
+                                   )
+                                })
+                            }
+                        </div>
+                        <div></div>
+                    </div>
+                    <div className="h-10 w-full bg-[#09090e]"></div>
+                    <div className="w-full bg-[#0c0c13]">345</div>
+                </div>
+            </div>
         </div>
     );
 }
---
+++ src/configs/launcher.tsx
@@ -0,0 +1,16 @@
+import {Icon} from "@iconify/react";
+
+export const LAUNCHER_TABS = [
+    {
+        icon: (
+            <Icon color="#fff" fontSize={24} icon="fluent:add-square-16-regular" />
+        ),
+        name: 'launcher.add-instance',
+    },
+    {
+        icon: (
+            <Icon color="#fff" fontSize={24} icon="fluent:add-square-16-regular" />
+        ),
+        name: 'launcher.folders'
+    }
+];
