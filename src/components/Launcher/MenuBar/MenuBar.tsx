994109a03d2476dccdeb9d0028c59094c36cc27c
windstone-aristotle-yellow <notwindstone@gmail.com>
2024-12-30 23:03:13 +0300

    feat: add right click context menu for menu bar

--- src/components/Launcher/MenuBar/MenuBar.tsx
+++ src/components/Launcher/MenuBar/MenuBar.tsx
@@ -2,14 +2,27 @@ import {LAUNCHER_TABS} from "@/configs/launcher";
 import Image from "next/image";
 import skinAvatar from "../../../../public/windstone_profile_skin.png";
 import {useTranslations} from "next-intl";
+import {useState} from "react";
+import {useClickOutside} from "@mantine/hooks";
 
 export default function MenuBar() {
     const translate = useTranslations('Translations');
+    const [opened, setOpened] = useState(false);
+    const ref = useClickOutside(() => setOpened(false));
 
     return (
         <div
+            onContextMenu={() => setOpened(true)}
             className="flex justify-between p-2.5 h-14 w-full bg-[#11111b]"
         >
+            {opened && (
+                <div
+                    ref={ref}
+                    className="absolute flex flex-col gap-2 border-[#181822] border-[1px] p-2 bg-[#11111B]"
+                >
+
+                </div>
+            )}
             <div className="flex items-stretch gap-2">
                 <div className="cursor-move w-[5px] rounded-full bg-[#dbcafe]"/>
                 {
