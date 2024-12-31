584a3f4ded4d735b2f776bf992a4bac133fd0b10
windstone-aristotle-yellow <notwindstone@gmail.com>
2024-12-31 23:33:20 +0300

    feat: change styles

--- src/components/Features/Features.tsx
+++ src/components/Features/Features.tsx
@@ -7,7 +7,7 @@ export default function Features() {
     const translate = useTranslations('Translations');
 
     return (
-        <div className="flex gap-4 justify-between items-start mt-12 max-w-[960px] px-4 mx-auto">
+        <div className="flex gap-8 justify-between items-start mt-12 max-w-[960px] px-4 mx-auto">
             <div className="flex-1 flex flex-col gap-2">
                 <div className="flex items-center text-white gap-2">
                     <Icon height={40} icon="fluent:people-community-16-regular"/>
