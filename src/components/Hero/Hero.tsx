ff96a123303173ed616f7b9779c5dffa5988f05b
windstone-aristotle-yellow <notwindstone@gmail.com>
2024-12-31 19:13:54 +0300

    feat: show platform in Hero

--- src/components/Hero/Hero.tsx
+++ src/components/Hero/Hero.tsx
@@ -1,9 +1,13 @@
 import Launcher from "@/components/Launcher/Launcher";
 
 export default function Hero() {
-    return (
-        <div className="max-w-[960px] mx-auto">
+    const platform = navigator.platform.toLowerCase();
 
+    return (
+        <div className="flex flex-col max-w-[960px] mx-auto">
+            <p className="text-center text-balance text-6xl text-white">
+                Download Freesm Launcher for {platform}
+            </p>
             <Launcher />
         </div>
     )
