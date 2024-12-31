5f0a623ccf88bf36ad3215c511affc8f54f4fb36
windstone <notwindstone@gmail.com>
2025-01-01 00:20:03 +0300

    fix(launcher): round close button

--- src/components/Launcher/WindowHeader/WindowHeader.tsx
+++ src/components/Launcher/WindowHeader/WindowHeader.tsx
@@ -25,7 +25,7 @@ export default function WindowHeader() {
                         className="transition w-[10px] h-[10px] border-[1px] border-[#999] group-hover:border-[#000]"
                     />
                 </div>
-                <div className="group flex justify-center items-center transition w-12 hover:bg-[#e81123]">
+                <div className="group flex justify-center items-center transition w-12 hover:bg-[#e81123] rounded-tr-md">
                     <div className="relative right-[6px]">
                         <div
                             className="absolute top-[50%] left-[50%] rotate-45 transition w-[13px] h-[1px] bg-[#999] group-hover:bg-[#fff]"
