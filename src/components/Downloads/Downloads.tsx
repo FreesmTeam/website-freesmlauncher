95b20f552b3b29a15796711c30d0c24e771c0765
windstone <notwindstone@gmail.com>
2025-01-01 00:42:54 +0300

    feat: implement basic segmented control in Downloads component

--- src/components/Downloads/Downloads.tsx
+++ src/components/Downloads/Downloads.tsx
@@ -2,12 +2,13 @@
 
 import { DOWNLOADS_OPTIONS } from "@/configs/constants";
 import getPlatformName from "@/utils/getPlatformName";
+import { useState } from "react";
 
 export default function Downloads() {
     const platform = navigator.platform.toLowerCase();
     const displayPlatform = getPlatformName(platform);
 
-    
+    const [selectedPlatform, setSelectedPlatform] = useState(displayPlatform);
 
     return (
         <div className="flex flex-col gap-8 mt-12 max-w-[960px] px-4 mx-auto">
@@ -17,13 +18,29 @@ export default function Downloads() {
             <p className="text-center text-balance text-2xl text-gray-400">
                 {('hero.description')}
             </p>
-            <div className="rounded-md border-[1px] border-[#181825] w-full bg-[#11111b] p-2">
+            <div className="flex gap-2 rounded-md border-[1px] border-[#181825] w-full bg-[#11111b] p-2">
                 {
                     DOWNLOADS_OPTIONS.map((option) => {
+                        if (option === selectedPlatform) {
+                            return (
+                                <button
+                                    onClick={() => setSelectedPlatform(option)}
+                                    className="px-2 py-1 flex-1 bg-[#1D1A28] flex justify-center font-semibold text-2xl items-center rounded-md" 
+                                    key={option}
+                                >
+                                    {option}
+                                </button>
+                            );
+                        }
+                        
                         return (
-                            <div key={option}>
+                            <button 
+                                onClick={() => setSelectedPlatform(option)}
+                                className="flex justify-center items-center px-2 py-1 text-2xl flex-1" 
+                                key={option}
+                            >
                                 {option}
-                            </div>
+                            </button>
                         );
                     })
                 }
