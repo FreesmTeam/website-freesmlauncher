88886d6149da4eabed3e832883d02ce9f1c8d580
windstone-aristotle-yellow <notwindstone@gmail.com>
2024-12-31 17:23:52 +0300

    feat(launcher): map instance bar items as react components

--- src/components/Launcher/InstanceBar/InstanceBar.tsx
+++ src/components/Launcher/InstanceBar/InstanceBar.tsx
@@ -2,8 +2,12 @@ import {useLauncherBarsStore} from "@/utils/stores";
 import {LauncherBarType} from "@/types/LauncherBar.type";
 import Image from "next/image";
 import grassSvg from '../../../../public/grass.svg';
+import {LAUNCHER_INSTANCE_BAR_ITEMS} from "@/configs/launcher";
+import {LauncherInstanceBarItemType} from "@/types/LauncherInstanceBarItem.type";
+import {useTranslations} from "next-intl";
 
 export default function InstanceBar() {
+    const translate = useTranslations('Translations');
     const launcherBarsStore = useLauncherBarsStore((state) => state);
     const instanceBar = launcherBarsStore.entries.find((entry: LauncherBarType) => entry.name === 'launcher.instance-toolbar');
     const lastIndex = launcherBarsStore.entries.length - 1;
@@ -19,15 +23,29 @@ export default function InstanceBar() {
                                 <div className="cursor-move rounded-full h-[5px] w-full bg-[#dbcafe]" />
                             )
                         }
-                        <div className="flex justify-center items-center rounded-md hover:bg-[#1b1825]">
+                        <div className="select-none flex justify-center items-center rounded-md hover:bg-[#1b1825]">
                             <Image width={80} src={grassSvg} alt="Grass svg icon" />
                         </div>
-                        <div className="flex justify-center items-center rounded-md hover:bg-[#1b1825]">
+                        <div className="select-none flex justify-center items-center rounded-md hover:bg-[#1b1825]">
                             <p className="text-[13px] text-[#CDD6F4]">
                                 1.21
                             </p>
                         </div>
-
+                        {
+                            LAUNCHER_INSTANCE_BAR_ITEMS.map((item: LauncherInstanceBarItemType) => {
+                                return (
+                                    <div
+                                        key={item.name}
+                                        className="select-none flex gap-1 items-center rounded-md hover:bg-[#1b1825]"
+                                    >
+                                        {item.icon}
+                                        <p className="text-[13px] text-[#CDD6F4]">
+                                            {translate(item.name)}
+                                        </p>
+                                    </div>
+                                );
+                            })
+                        }
                     </div>
                 )
             }
