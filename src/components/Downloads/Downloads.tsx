f985a5978d8531270e8696a259a3975edd7fc418
windstone <notwindstone@gmail.com>
2025-01-01 00:48:04 +0300

    feat: translate Downloads page

--- src/components/Downloads/Downloads.tsx
+++ src/components/Downloads/Downloads.tsx
@@ -2,9 +2,12 @@
 
 import { DOWNLOADS_OPTIONS } from "@/configs/constants";
 import getPlatformName from "@/utils/getPlatformName";
+import { useTranslations } from "next-intl";
 import { useState } from "react";
 
 export default function Downloads() {
+    const translate = useTranslations('Translations');
+
     const platform = navigator.platform.toLowerCase();
     const displayPlatform = getPlatformName(platform);
 
@@ -13,10 +16,10 @@ export default function Downloads() {
     return (
         <div className="flex flex-col gap-8 mt-12 max-w-[960px] px-4 mx-auto">
             <p className="text-center font-bold text-balance text-7xl text-white">
-                Downloads
+                {translate('downloads.title')}
             </p>
             <p className="text-center text-balance text-2xl text-gray-400">
-                {('hero.description')}
+                {translate('downloads.description')}
             </p>
             <div className="flex gap-2 rounded-md border-[1px] border-[#181825] w-full bg-[#11111b] p-2">
                 {
@@ -25,7 +28,7 @@ export default function Downloads() {
                             return (
                                 <button
                                     onClick={() => setSelectedPlatform(option)}
-                                    className="px-2 py-1 flex-1 bg-[#1D1A28] flex justify-center font-semibold text-2xl items-center rounded-md" 
+                                    className="transition px-2 py-1 flex-1 bg-[#1D1A28] flex justify-center text-2xl items-center rounded-md" 
                                     key={option}
                                 >
                                     {option}
@@ -36,7 +39,7 @@ export default function Downloads() {
                         return (
                             <button 
                                 onClick={() => setSelectedPlatform(option)}
-                                className="flex justify-center items-center px-2 py-1 text-2xl flex-1" 
+                                className="transition flex justify-center items-center px-2 py-1 text-2xl flex-1" 
                                 key={option}
                             >
                                 {option}
