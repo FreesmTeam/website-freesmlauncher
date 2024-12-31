6209160b65a45716f4df52809b18af41dc33049b
windstone-aristotle-yellow <notwindstone@gmail.com>
2024-12-31 19:56:53 +0300

    feat: add translations to Hero component

--- src/components/Hero/Hero.tsx
+++ src/components/Hero/Hero.tsx
@@ -3,8 +3,10 @@
 import Launcher from "@/components/Launcher/Launcher";
 import Link from "next/link";
 import {DOWNLOADS_LINK, GITHUB_LINK} from "@/configs/constants";
+import {useTranslations} from "next-intl";
 
 export default function Hero() {
+    const translate = useTranslations('Translations');
     const platform = navigator.platform.toLowerCase();
     let displayPlatform;
 
@@ -26,17 +28,17 @@ export default function Hero() {
     return (
         <div className="flex flex-col gap-8 mt-12 max-w-[960px] px-4 mx-auto">
             <p className="text-center font-bold text-balance text-7xl text-white">
-                Download Freesm Launcher for{' '}
+                {translate('hero.title')}{' '}
                 <span className="text-[#cba6f7]">
                     {displayPlatform}
                 </span>
             </p>
             <p className="text-center text-balance text-2xl text-gray-400">
-                Freesm Launcher is a custom launcher for Minecraft that allows you to play with offline account without any restrictions.
+                {translate('hero.description')}
             </p>
             <div className="flex gap-2 justify-center">
                 <Link href={DOWNLOADS_LINK} className="bg-[#cba6f7] py-2 px-4 rounded-md font-bold text-[18px] text-black transition hover:bg-[#8839ef] hover:text-white">
-                    Download Now
+                    {translate('hero.download-now')}
                 </Link>
                 <Link target="_blank" href={GITHUB_LINK} className="transition border-white border-[1px] bg-transparent py-2 px-4 rounded-md font-bold text-[18px] text-white hover:text-[#cba6f7] hover:border-[#cba6f7]">
                     Github
