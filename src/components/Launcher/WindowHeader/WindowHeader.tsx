4622c58cb5066e356f8f7b6129da9a0191356b0f
windstone <notwindstone@gmail.com>
2025-01-02 01:30:07 +0300

    feat: adapt WindowHeader to mobile screens

--- src/components/Launcher/WindowHeader/WindowHeader.tsx
+++ src/components/Launcher/WindowHeader/WindowHeader.tsx
@@ -3,35 +3,35 @@ import freesmLogo from "../../../../public/favicon.webp";
 
 export default function WindowHeader() {
     return (
-        <div className="flex rounded-t-md justify-between items-center w-full h-8 bg-[#11111b]">
-            <div className="select-none px-2 flex gap-2 items-center">
+        <div className="flex rounded-t-md justify-between items-center w-full h-6 sm:h-8 bg-[#11111b]">
+            <div className="select-none px-1 sm:px-2 flex gap-2 items-center">
                 <Image
-                    className="h-5 w-5"
+                    className="w-4 h-4 sm:h-5 sm:w-5"
                     src={freesmLogo}
                     alt={""}
                 />
-                <div className="text-sm text-white">
+                <div className="text-xs sm:text-sm text-white">
                     Freesm Launcher
                 </div>
             </div>
             <div className="flex h-full gap-0 items-stretch">
-                <div className="group flex justify-center items-center transition w-12 hover:bg-[#e5e5e5]">
+                <div className="group flex justify-center items-center transition w-8 sm:w-12 hover:bg-[#e5e5e5]">
                     <div
-                        className="transition w-[10px] h-[1px] bg-[#999] group-hover:bg-[#000]"
+                        className="transition w-[8px] sm:w-[10px] h-[1px] bg-[#999] group-hover:bg-[#000]"
                     />
                 </div>
-                <div className="group flex justify-center items-center transition w-12 hover:bg-[#e5e5e5]">
+                <div className="group flex justify-center items-center transition w-8 sm:w-12 hover:bg-[#e5e5e5]">
                     <div
-                        className="transition w-[10px] h-[10px] border-[1px] border-[#999] group-hover:border-[#000]"
+                        className="transition w-[8px] sm:w-[10px] h-[8px] sm:h-[10px] border-[1px] border-[#999] group-hover:border-[#000]"
                     />
                 </div>
-                <div className="group flex justify-center items-center transition w-12 hover:bg-[#e81123] rounded-tr-md">
+                <div className="group flex justify-center items-center transition w-8 sm:w-12 hover:bg-[#e81123] rounded-tr-md">
                     <div className="relative right-[6px]">
                         <div
-                            className="absolute top-[50%] left-[50%] rotate-45 transition w-[13px] h-[1px] bg-[#999] group-hover:bg-[#fff]"
+                            className="absolute top-[50%] left-[50%] rotate-45 transition w-[10px] sm:w-[13px] h-[1px] bg-[#999] group-hover:bg-[#fff]"
                         />
                         <div
-                            className="absolute top-[50%] left-[50%] rotate-[135deg] transition w-[13px] h-[1px] bg-[#999] group-hover:bg-[#fff]"
+                            className="absolute top-[50%] left-[50%] rotate-[135deg] transition w-[10px] sm:w-[13px] h-[1px] bg-[#999] group-hover:bg-[#fff]"
                         />
                     </div>
                 </div>
