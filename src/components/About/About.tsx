799c6671584c5c10d4ecdffdaf05d33259f48152
windstone-aristotle-yellow <notwindstone@gmail.com>
2025-01-01 20:35:45 +0300

    refactor: add maintainers in separate json file, which data is mapped into React nodes

--- src/components/About/About.tsx
+++ src/components/About/About.tsx
@@ -3,9 +3,13 @@
 import {useTranslations} from "next-intl";
 import Image from "next/image";
 import Link from "next/link";
+import community from '@/configs/community.json';
 
 export default function About() {
     const translate = useTranslations('Translations');
+    const info = useTranslations('Info');
+    const locale = info('locale');
+    const maintainers = community.maintainers;
 
     return (
         <div className="flex flex-col gap-8 mt-12 max-w-[960px] px-4 mx-auto">
@@ -20,52 +24,34 @@ export default function About() {
                     Maintainers
                 </p>
                 <div className="flex gap-4 justify-center">
-                    <Link
-                        target="_blank"
-                        href="https://github.com/notwindstone/"
-                        className="transition hover:grayscale  rounded-md p-2 flex flex-1 gap-2 items-start"
-                    >
-                        <Image
-                            className="rounded-full"
-                            width={96}
-                            height={96}
-                            src={'https://avatars.githubusercontent.com/u/91563727?v=4'}
-                            alt={'windstone avatar'}
-                        />
-                        <div className="flex flex-col">
-                            <p className="text-[#cba6f7] font-semibold text-[24px]">
-                                windstone
-                            </p>
-                            <p>
-                                OSS Enthusiast, creator of the idea.
-                                Nix stuff maintainer
-                                also known as hand7s
-                            </p>
-                        </div>
-                    </Link>
-                    <Link
-                        target="_blank"
-                        href="https://github.com/s0me1newithhand7s/"
-                        className="transition hover:grayscale  rounded-md p-2 flex flex-1 gap-2 items-start"
-                    >
-                        <Image
-                            className="rounded-full"
-                            width={96}
-                            height={96}
-                            src={'https://avatars.githubusercontent.com/u/117505144?v=4'}
-                            alt={'hand7s avatar'}
-                        />
-                        <div className="flex flex-col">
-                            <p className="text-[#cba6f7] font-semibold text-[24px]">
-                                s0me1newithhand7s
-                            </p>
-                            <p>
-                                OSS Enthusiast, creator of the idea.
-                                Nix stuff maintainer
-                                also known as hand7s
-                            </p>
-                        </div>
-                    </Link>
+                    {
+                        maintainers.map((maintainer) => {
+                            return (
+                                <Link
+                                    key={maintainer.name}
+                                    target="_blank"
+                                    href={maintainer.link}
+                                    className="transition hover:grayscale rounded-md p-2 flex flex-1 gap-4 items-start"
+                                >
+                                    <Image
+                                        className="rounded-full"
+                                        width={96}
+                                        height={96}
+                                        src={maintainer.avatar}
+                                        alt={'windstone avatar'}
+                                    />
+                                    <div className="flex flex-col">
+                                        <p className="text-[#cba6f7] font-semibold text-[24px]">
+                                            {maintainer.name}
+                                        </p>
+                                        <p className="text-justify text-white">
+                                            {maintainer.descriptionEn}
+                                        </p>
+                                    </div>
+                                </Link>
+                            );
+                        })
+                    }
                 </div>
             </div>
         </div>
