61e52fa06250001865691a341b237d1d6272cf54
windstone-aristotle-yellow <notwindstone@gmail.com>
2025-01-03 18:47:09 +0300

    feat: show different metadata based on page

--- src/app/[locale]/about/page.tsx
+++ src/app/[locale]/about/page.tsx
@@ -1,4 +1,25 @@
 import About from "@/components/About/About";
+import {Metadata} from "next";
+import {getTranslations} from "next-intl/server";
+import {APP_NAME} from "@/configs/constants";
+
+export async function generateMetadata(): Promise<Metadata> {
+    const translate = await getTranslations('Translations');
+
+    const title = translate('pages.about.title');
+    const description = translate('pages.about.description');
+
+    return {
+        title: title,
+        description: description,
+        openGraph: {
+            siteName: APP_NAME,
+            type: "website",
+            title: title,
+            description: description,
+        }
+    };
+}
 
 export default function Page() {
     return (
--- src/app/[locale]/downloads/page.tsx
+++ src/app/[locale]/downloads/page.tsx
@@ -1,4 +1,25 @@
 import Downloads from "@/components/Downloads/Downloads";
+import {Metadata} from "next";
+import {getTranslations} from "next-intl/server";
+import {APP_NAME} from "@/configs/constants";
+
+export async function generateMetadata(): Promise<Metadata> {
+    const translate = await getTranslations('Translations');
+
+    const title = translate('pages.downloads.title');
+    const description = translate('pages.downloads.description');
+
+    return {
+        title: title,
+        description: description,
+        openGraph: {
+            siteName: APP_NAME,
+            type: "website",
+            title: title,
+            description: description,
+        }
+    };
+}
 
 export default function Page() {
     return (
