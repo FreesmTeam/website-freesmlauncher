6e8d63f6e4633c4d6ad91ef6c1959646d094a37a
windstone-aristotle-yellow <notwindstone@gmail.com>
2025-01-08 02:45:29 +0300

    feat: add successful-login page

---
+++ src/app/[locale]/successful-login/page.tsx
@@ -0,0 +1,35 @@
+import {Metadata} from "next";
+import {getTranslations} from "next-intl/server";
+import {APP_NAME} from "@/configs/constants";
+
+export async function generateMetadata(): Promise<Metadata> {
+    const translate = await getTranslations('Translations');
+
+    const title = translate('pages.successful-login.title');
+    const description = translate('pages.successful-login.description');
+
+    return {
+        title: title,
+        description: description,
+        openGraph: {
+            images: "/banner.webp",
+            siteName: APP_NAME,
+            type: "website",
+            title: title,
+            description: description,
+        },
+        twitter: {
+            card: "summary",
+            title: title,
+            description: description,
+        },
+    };
+}
+
+export default function Page() {
+    return (
+        <>
+            success
+        </>
+    );
+}
