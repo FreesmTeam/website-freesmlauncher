dae40e0063f823cc009a221fd828de20a1c0d2b9
windstone-aristotle-yellow <notwindstone@gmail.com>
2025-01-08 03:13:43 +0300

    feat: show maxwell cat gif on successful login

--- src/components/SuccessfulLogin/SuccessfulLogin.tsx
+++ src/components/SuccessfulLogin/SuccessfulLogin.tsx
@@ -1,16 +1,19 @@
 import {useTranslations} from "next-intl";
+import maxwellGif from '../../../public/maxwell-christmas.gif';
+import Image from "next/image";
 
 export default function SuccessfulLogin() {
     const translate = useTranslations('Translations');
 
     return (
-        <div className="flex flex-col gap-8 mt-12 max-w-[960px] px-4 mx-auto">
+        <div className="flex flex-col items-center gap-8 mt-12 max-w-[960px] px-4 mx-auto">
             <p className="text-center font-bold text-balance text-5xl sm:text-7xl text-white">
                 {translate('successful-login.title')}
             </p>
             <p className="text-center text-balance text-lg sm:text-2xl text-gray-400">
                 {translate('successful-login.description')}
             </p>
+            <Image src={maxwellGif} alt={"Maxwell cat rotating gif"} />
         </div>
     );
 }
