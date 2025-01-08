6f351b4cb2dc0e53debc049a3d566585b7fa95fb
windstone-aristotle-yellow <notwindstone@gmail.com>
2025-01-08 16:10:38 +0300

    feat: add shadow to global-overlay class

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -2,7 +2,7 @@ export default function Footer() {
     return (
         <>
             <div className="h-20" />
-            <div className="text-white fixed bottom-0 pb-20 sm:pb-0 -z-50">
+            <div className=" bg-[#11111b] h-48 sm:h-28 w-full px-4 pt-4 fixed bottom-0 pb-20 sm:pb-0 -z-50">
                 footer
                 footer
                 footer
