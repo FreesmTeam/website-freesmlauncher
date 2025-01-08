dbd4d0064f98e5aa801eb1af79475ff657e15136
windstone-aristotle-yellow <notwindstone@gmail.com>
2025-01-08 18:04:08 +0300

    feat: change footer styles

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -9,13 +9,13 @@ export default function Footer() {
             <div className="-z-50 h-[200px]" />
             <div className="flex justify-center bg-[#1b1b2a] h-[312px] sm:h-[232px] w-full px-4 pt-8 fixed bottom-0 pb-[112px] sm:pb-8 z-0">
                 <div className="max-w-[1280px] w-full flex flex-col flex-nowrap sm:flex-row sm:justify-between gap-4">
-                    <div className="flex flex-col gap-2 items-center sm:items-start">
+                    <div className="select-none flex flex-col gap-2 items-center sm:items-start">
                         <Image
                             height={40}
                             src={websiteLogo}
                             alt={"Freesm Launcher logo"}
                         />
-                        <p className="text-gray-400 text-[16px] sm:text-[18px]">
+                        <p className="text-gray-400 text-[14px] sm:text-[16px]">
                             Another way to play Minecraft for free.
                         </p>
                     </div>
@@ -25,9 +25,9 @@ export default function Footer() {
                                 return (
                                     <div
                                         key={item.title}
-                                        className="px-4 flex flex-1 flex-col gap-2 items-center sm:items-start"
+                                        className="select-none px-4 flex flex-1 flex-col gap-2 items-center sm:items-start"
                                     >
-                                        <p className="text-white font-semibold text-[18px] sm:text-[20px]">
+                                        <p className="text-white font-semibold text-[16px] sm:text-[18px]">
                                             {item.title}
                                         </p>
                                         {
@@ -37,7 +37,7 @@ export default function Footer() {
                                                         href={link.link}
                                                         target="_blank"
                                                         key={link.name}
-                                                        className="leading-5 text-wrap text-gray-400 text-[16px] sm:text-[18px]"
+                                                        className="leading-5 text-wrap text-gray-400 text-[14px] sm:text-[16px] underline-offset-[5px] hover:underline"
                                                     >
                                                         {link.name}
                                                     </Link>
