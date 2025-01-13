6dede112ad63c66db4422c7a4a08481709633592
windstone-aristotle-yellow <notwindstone@gmail.com>
2025-01-13 20:25:26 +0300

    chore(deps): replace next-base64-encoder with next-base64 package

--- src/app/feed/route.ts
+++ src/app/feed/route.ts
@@ -1,4 +1,4 @@
-import { toBytes } from "fast-base64";
+import nextBase64 from 'next-base64';
 
 export const dynamic = "force-dynamic";
 
@@ -6,13 +6,14 @@ export async function GET() {
     const feedResponse = await fetch("https://api.github.com/repos/freesmteam/news/contents/feed.md");
 
     const news = await feedResponse.text();
+    const parsedNews = JSON.parse(news);
+    const decodedNews = nextBase64.decode(parsedNews?.content);
+    console.log(decodedNews.split('---'));
 
     const response = new Response(news, {
         status: 200,
         statusText: "ok",
     });
-
-    let bytes = await toBytes('SGVsbG8sIHdvcmxkIQ==');
 
     response.headers.append("content-type", "text/xml");
 
