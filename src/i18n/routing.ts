451cacce171a4e041df3acd5e2ddf5f51452f07c
windstone-aristotle-yellow <notwindstone@gmail.com>
2025-01-14 13:10:30 +0300

    refactor: move locales list to a separate json file

--- src/i18n/routing.ts
+++ src/i18n/routing.ts
@@ -1,9 +1,10 @@
 import {defineRouting} from 'next-intl/routing';
 import {createNavigation} from 'next-intl/navigation';
+import locales from '@/configs/locales.json';
 
 export const routing = defineRouting({
     // A list of all locales that are supported
-    locales: ['en', 'ru'],
+    locales: locales,
 
     // Used when no locale matches
     defaultLocale: 'en'
