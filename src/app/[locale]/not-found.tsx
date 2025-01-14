5428b57ed1dc403317f93dd9a53e70af79c0f640
windstone-aristotle-yellow <notwindstone@gmail.com>
2025-01-14 17:50:04 +0300

    feat: add custom not-found page

--- src/app/[locale]/not-found.tsx
+++ src/app/[locale]/not-found.tsx
@@ -1,11 +1,25 @@
 import Link from 'next/link';
+import {useTranslations} from "next-intl";
 
 export default function NotFound() {
+    const translate = useTranslations('Translations');
+
     return (
-        <div>
-            <h2>Not Found</h2>
-            <p>Could not find requested resource</p>
-            <Link href="/">Return Home</Link>
+        <div className="flex text-balance text-center justify-center items-center h-[calc(100svh-106px)] w-full max-w-[900px] px-4 sm:px-6 mx-auto flex-col text-white gap-2">
+            <p className="text-[#cba6f7] text-6xl sm:text-9xl font-black pb-2 sm:pb-4">
+                {translate('pages.not-found.title')}
+            </p>
+            <p className="text-xl sm:text-3xl font-bold">
+                {translate('pages.not-found.subtitle')}
+            </p>
+            <p className="text-sm sm:text-lg">
+                {translate('pages.not-found.description')}
+            </p>
+            <Link href="/">
+                <p>
+                    {translate('pages.not-found.go-to-home')}
+                </p>
+            </Link>
         </div>
     );
 }
