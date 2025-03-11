03ae5047b7272fbcb306d5d5fea077f0d70d8168
notwindstone <notwindstone@gmail.com>
2025-03-11 16:22:18 +0300

    feat: implement better /feed api route logic

--- src/app/feed/route.ts
+++ src/app/feed/route.ts
@@ -1,12 +1,25 @@
 import nextBase64 from 'next-base64';
 import prismNews from '@/configs/prismNews.json';
+import addUser from "@/lib/addUser";
+import {NextRequest, userAgent} from "next/server";
 
 export const dynamic = "force-dynamic";
 
 // sorry i don't wanna use another library to generate fucking feed.xml file for launcher
 // and i really hate .xml files so i didn't bother with the quality of my code
-export async function GET() {
+export async function GET(request: NextRequest) {
+    const useragent = userAgent(request).ua;
+
     const githubContents = await fetch("https://api.github.com/repos/freesmteam/news/contents/feed.md");
+
+    if (!githubContents.ok) {
+        return new Response(
+            prismNews.beforeContent + prismNews.afterContent,
+            {
+                status: 429,
+            }
+        );
+    }
 
     const news = await githubContents.text();
     const parsedNews = JSON.parse(news);
@@ -51,6 +64,14 @@ export async function GET() {
     );
 
     response.headers.append("content-type", "text/xml");
+
+    // don't wait for database response, just return xml
+    // in case it fails we don't really care
+    addUser({
+        useragent,
+    }).catch((err) => {
+        console.error(err.message);
+    });
 
     return response;
 }
