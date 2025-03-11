6d3aa80dcea6829ad29f71dcd292e5e85c16abbf
notwindstone <notwindstone@gmail.com>
2025-03-11 16:05:48 +0300

    feat: set up drizzle db code

---
+++ src/db/schema.ts
@@ -0,0 +1,14 @@
+import {integer, pgTable, text, timestamp, varchar} from "drizzle-orm/pg-core";
+
+export const usersTable = pgTable("users", {
+    id: integer('id').primaryKey().generatedAlwaysAsIdentity(),
+    useragent: text('useragent').notNull(),
+    createdAt: timestamp('created_at', {
+        withTimezone: true,
+        mode: "date",
+    }).notNull(),
+    lastSeen: timestamp('last_seen', {
+        withTimezone: true,
+        mode: "date",
+    }).notNull(),
+});
