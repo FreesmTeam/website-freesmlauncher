7eb7517b9cd4b94f69e2fcfdb64c8ed426775890
notwindstone <notwindstone@gmail.com>
2025-03-12 17:50:36 +0300

    feat: increment times_seen counter on every /feed request

--- src/lib/addUser.ts
+++ src/lib/addUser.ts
@@ -1,6 +1,7 @@
 import 'dotenv/config';
 import { usersTable } from '@/db/schema';
 import {db} from "@/db";
+import {sql} from "drizzle-orm";
 
 export default async function addUser({
     useragent,
@@ -13,11 +14,13 @@ export default async function addUser({
             useragent,
             createdAt: new Date(),
             lastSeen: new Date(),
+            timesSeen: 1,
         })
         .onConflictDoUpdate({
             target: usersTable.useragent,
             set: {
                 lastSeen: new Date(),
+                timesSeen: sql`${usersTable.timesSeen} + 1`,
             },
         });
 }
