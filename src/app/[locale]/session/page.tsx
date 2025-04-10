739b5bbfb820aab3e31aa3b01d4a0f823e27ac2c
notwindstone <notwindstone@gmail.com>
2025-04-10 17:50:07 +0300

    feat: add sign in page

---
+++ src/app/[locale]/session/page.tsx
@@ -0,0 +1,23 @@
+import { Icon } from "@iconify/react";
+
+export default function Page() {
+    return (
+        <div className="flex text-white p-8 h-[50vh]">
+            <a
+                className="bg-zinc-900 border-zinc-100 border-[1px] rounded-md w-32 h-12 flex justify-center items-center transition hover:bg-zinc-800"
+                href={"/api/oauth2/login/github"}
+            >
+                <div className="flex flex-nowrap items-center gap-2">
+                    <Icon
+                        className="text-white"
+                        height={24}
+                        icon={"mdi:github"}
+                    />
+                    <p className="text-lg font-semibold">
+                        Github
+                    </p>
+                </div>
+            </a>
+        </div>
+    );
+}
--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -23,6 +23,9 @@ export default function Footer() {
                         <p className="text-gray-400 text-[14px] sm:text-[16px]">
                             {translate('footer.short-description')}
                         </p>
+                        <Link className="text-xs sm:text-sm text-zinc-600 transition hover:text-zinc-500" href="/session">
+                            sign in?
+                        </Link>
                     </div>
                     <div className="flex gap-4 flex-col sm:flex-row">
                         {
