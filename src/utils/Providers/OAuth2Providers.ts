d09915b32ab50810ad88fddcb08b76a9a347b15a
notwindstone <notwindstone@gmail.com>
2025-04-10 17:55:41 +0300

    feat: remove redirection uri for github oauth2

--- src/utils/Providers/OAuth2Providers.ts
+++ src/utils/Providers/OAuth2Providers.ts
@@ -6,6 +6,6 @@ export const GitHubProvider = async () => {
     return new arctic.GitHub(
         process.env.GITHUB_CLIENT_ID!,
         process.env.GITHUB_SECRET_KEY!,
-        "https://website-freesmlauncher.vercel.app/",
+        null,
     );
 };
