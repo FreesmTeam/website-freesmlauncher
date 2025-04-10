f81365c131594c2f89137ec2432fb6f58107c6d8
notwindstone <notwindstone@gmail.com>
2025-04-10 17:51:48 +0300

    fix: fix middleware routing to api routes with locale

--- src/middleware.ts
+++ src/middleware.ts
@@ -7,8 +7,8 @@ export const config = {
     matcher: [
         // Exclude files with a "." followed by an extension, which are typically static files.
         // Exclude files in the _next directory, which are Next.js internals.
-        "/((?!.+\\.[\\w]+$|_next|feed).*)",
+        "/((?!.+\\.[\\w]+$|_next|feed|api).*)",
         // Re-include any files in the api or trpc folders that might have an extension
-        "/(api|trpc)(.*)"
+        "/(trpc)(.*)"
     ]
 };
